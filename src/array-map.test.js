a6b7f04a42c855643d783374899aba908202a593
Katherine Tam <katherinemtam@gmail.com>
2021-04-26 16:15:24 -0700

    tested stringItUp function

--- src/array-map.test.js
+++ src/array-map.test.js
@@ -1,7 +1,13 @@
-import { doubleNumbers } from './array-map';
+import { doubleNumbers, stringItUp } from './array-map';
 
 test('doubles numbers', () => {
   const input = [2, 5, 100]; // arrange
   const output = doubleNumbers(input); // act
   expect(output).toEqual([4, 10, 200]); // assert
+});
+
+test('string it up', () => {
+  const input = [2, 5, 100]; // arrange
+  const output = stringItUp(input); // act
+  expect(output).toEqual(['2', '5', '100']); // assert
 });
