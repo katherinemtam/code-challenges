c3b7e1679ddfc3fcaf1443c308217123ee9e72f4
Katherine Tam <katherinemtam@gmail.com>
2021-04-26 16:56:25 -0700

    tested makeStrings function

--- src/array-map.js
+++ src/array-map.js
@@ -13,3 +13,11 @@ export function stringItUp(arr) {
 export function namesOnly(arr) {
   return arr.map(n => n.name);
 }
+
+export function makeStrings(arr) {
+  return arr.map(n => {
+    if (n.age >= 18)
+      return `${n.name} can go to The Matrix`;
+    else return `${n.name} is under age!!`;
+  });
+}
--- src/array-map.test.js
+++ src/array-map.test.js
@@ -1,4 +1,4 @@
-import { doubleNumbers, stringItUp, namesOnly } from './array-map';
+import { doubleNumbers, stringItUp, namesOnly, makeStrings } from './array-map';
 
 test('doubles numbers', () => {
   const input = [2, 5, 100]; // arrange
@@ -48,5 +48,38 @@ test('names only', () => {
     'Paris Hilton',
     'Kayne West',
     'Bob Ziroll'
+  ]); // assert
+});
+
+test('make strings', () => {
+  const input = [
+    {
+      name: 'Angelina Jolie',
+      age: 80
+    },
+    {
+      name: 'Eric Jones',
+      age: 2
+    },
+    {
+      name: 'Paris Hilton',
+      age: 5
+    },
+    {
+      name: 'Kayne West',
+      age: 16
+    },
+    {
+      name: 'Bob Ziroll',
+      age: 100
+    }
+  ]; // arrange
+  const output = makeStrings(input); // act
+  expect(output).toEqual([
+    'Angelina Jolie can go to The Matrix',
+    'Eric Jones is under age!!',
+    'Paris Hilton is under age!!',
+    'Kayne West is under age!!',
+    'Bob Ziroll can go to The Matrix'
   ]); // assert
 });
