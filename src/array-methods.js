063850a18acbd6246aa639074f9c2a4ec1e9948b
Katherine Tam <katherinemtam@gmail.com>
2021-05-07 10:22:49 -0700

    tested howMuchPencil function

---
+++ src/array-methods.js
@@ -0,0 +1,36 @@
+export function howMuchPencil(str) {
+  let arr = [];
+  for (let i = 0; i <= str.length; i++) {
+    arr.push(str.slice(i));
+  }
+  return arr;
+}
+
+export function wordsToCharList(str) {
+
+}
+
+export function listFoods(recipe) {
+
+}
+
+export function stepActions(recipe) {
+
+}
+
+export function removeLastCharacters(str, numberOfCharacters) {
+
+}
+
+// stretch goals 
+export function totalSumCSV(str) {
+
+}
+
+export function removeVowels(str) {
+
+}
+
+export function extractVowels(str) {
+
+}
