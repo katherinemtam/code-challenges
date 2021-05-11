e6cf4cc4a897ac379b9adced97211d0dfcde45c9
Katherine Tam <katherinemtam@gmail.com>
2021-05-10 20:55:09 -0700

    tested findMax function

--- src/code-challenge-11.js
+++ src/code-challenge-11.js
@@ -3,7 +3,8 @@ export function returnTen(str) {
 }
 
 export function findMax(matrix) {
-    
+  const maxOfEachArray = matrix.map(arr => Math.max.apply(null, arr));
+  return Math.max.apply(null, maxOfEachArray);
 }
 
 export function totalSum(matrix) {
--- src/code-challenge-11.test.js
+++ src/code-challenge-11.test.js
@@ -1,11 +1,11 @@
 import { returnTen, findMax, totalSum, salesData, grandTotal } from './code-challenge-11';
 
-test.only('it should return the last 10 characters of a string as an array', () => {
+test('it should return the last 10 characters of a string as an array', () => {
   expect(returnTen('hello world')).toStrictEqual(['e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']);
   expect(returnTen('world')).toStrictEqual(['w', 'o', 'r', 'l', 'd']);
 });
 
-test('it should take in a matrix of positive numbers and return the number with the highest value', () => {
+test.only('it should take in a matrix of positive numbers and return the number with the highest value', () => {
   expect(findMax([[13, 24, 24, 2], [2, 5, 6], [2, 3]])).toStrictEqual(24);
 });
 
