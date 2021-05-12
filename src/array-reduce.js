cc4c37ed8f0ff4f5956db4d647e8315d90039b93
Katherine Tam <katherinemtam@gmail.com>
2021-05-11 21:48:46 -0700

    test addValues function

--- src/array-reduce.js
+++ src/array-reduce.js
@@ -1,3 +1,7 @@
 export const toLastNames = people => {
   return people.map(person => person.firstName + ' ' + person.lastName);
 };
+
+export const addValues = arr => {
+  return arr.reduce((total, value) => total += value, 0);
+};
--- src/array-reduce.test.js
+++ src/array-reduce.test.js
@@ -1,7 +1,13 @@
-import { toLastNames } from './array-reduce';
+import { toLastNames, addValues } from './array-reduce';
 
 test("iterates over an array of people objects and creates a new list of each person's full name using the array method 'map'", () => {
   const input = [{ firstName:'Jane', lastName:'Doe' }, { firstName:'James', lastName:'Bond' }];
   const output = ['Jane Doe', 'James Bond'];
   expect(toLastNames(input)).toStrictEqual(output);
+});
+
+test('uses reduce to add the values in the array', () => {
+  const input = [1, 2, 3, 4, 5];
+  const output = 15;
+  expect(addValues(input)).toStrictEqual(output);
 });
