c20757893a64717d3bc92bec2a193c34f423c75b
Katherine Tam <katherinemtam@gmail.com>
2021-05-12 23:05:25 -0700

    setup functions and test

---
+++ src/code-challenge-13.js
@@ -0,0 +1,19 @@
+export const sortByChildren = (charArray) => {
+
+};
+
+export const containsW = (str) => {
+
+};
+
+export const isNum = (input) => {
+
+};
+
+export const containsWorld = (input) => {
+
+};
+
+export const isCapitalized = (str) => {
+
+};
