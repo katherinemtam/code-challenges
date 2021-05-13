57cfd22b65197eeff5a38ee0b7cc7559b1e7bc7c
Katherine Tam <katherinemtam@gmail.com>
2021-05-12 23:06:02 -0700

    test sortByChildren function

--- src/code-challenge-13.js
+++ src/code-challenge-13.js
@@ -1,5 +1,9 @@
 export const sortByChildren = (charArray) => {
-
+  return charArray.sort((a, b) => {
+    const childrenLength = a.children.length - b.children.length;
+    if (childrenLength !== 0) return childrenLength;
+    return (a.name < b.name);
+  });
 };
 
 export const containsW = (str) => {
--- src/code-challenge-13.test.js
+++ src/code-challenge-13.test.js
@@ -45,7 +45,7 @@ let characters = [
   }
 ];
 
-describe('Testing challenge 1', () => {
+describe.only('Testing challenge 1', () => {
   test('It should sort the characters by number of children', () => {
     expect(sortByChildren(characters)[0].name).toStrictEqual('Euron');
     expect(sortByChildren(characters)[0].children.length).toStrictEqual(0);
